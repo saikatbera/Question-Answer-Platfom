f9e71955a142433032a3859d66cad4d964104c5e
saikatbera <36673548+saikatbera@users.noreply.github.com>
2018-02-20 23:11:05 +0530

    Online Question Answer Platform
    Compter Science Discussion Forum

---
+++ Discussion_forum/header.php
@@ -0,0 +1,58 @@
+<?php require_once("utility.php"); 
+ob_start(); 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+<title>Computer Science Discussion Forum</title>
+
+<script type="text/javascript" src="script.js"></script>
+
+    <link rel="stylesheet" href="res/style.css" type="text/css" media="screen" />
+    
+</head>
+
+<body>
+
+<div id="art-page-background-simple-gradient">
+    </div>
+    <div id="art-page-background-glare">
+        <div id="art-page-background-glare-image"></div>
+    </div>
+    <div id="art-main">
+        <div class="art-Sheet">
+            <div class="art-Sheet-tl"></div>
+            <div class="art-Sheet-tr"></div>
+            <div class="art-Sheet-bl"></div>
+            <div class="art-Sheet-br"></div>
+            <div class="art-Sheet-tc"></div>
+            <div class="art-Sheet-bc"></div>
+            <div class="art-Sheet-cl"></div>
+            <div class="art-Sheet-cr"></div>
+            <div class="art-Sheet-cc"></div>
+            <div class="art-Sheet-body">
+                <div class="art-Header">
+                    <div class="art-Hello-jpeg"></div>
+                    <div class="art-Logo">
+                        <h1 id="name-text" class="art-Logo-name"><a href="#">Computer Science Discussion Forum</a></h1>
+                    </div>
+                </div>
+                <div>
+                	<div class="l"></div>
+                	<div class="r"></div>
+                	<ul class="art-menu">
+                		<li><a href="uhome.php" id="auhome"><span class="l"></span><span class="r"></span><span class="t">Home</span></a></li>
+                		
+                		<li><a href="forum.php" id="aforum"><span class="l" ></span><span class="r"></span><span class="t">Forum</span></a>
+                			
+                		
+
+                    </ul>
+                </div>
+               <div class="art-contentLayout">
+                    <div class="art-content">
+                    </div>
+                </div>
+                    
+                      
---
+++ Discussion_forum/unreg.php
@@ -0,0 +1,6 @@
+<?php session_start();
+ require("header.php")?>
+ 
+ <h2>You are  not registered user!!!!<br/></h2>
+ <h3> <a href="register.php">Please register</a>
+ </h3>
